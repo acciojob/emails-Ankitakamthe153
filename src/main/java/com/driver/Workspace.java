1bc2d88a5976636a3d5575828d64002bda723230
Ankitakamthe153 <88307915+Ankitakamthe153@users.noreply.github.com>
2023-12-23 21:53:16 +0530

    Update Workspace.java

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -13,13 +13,13 @@ public class Workspace extends Gmail{
 
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
-        super(emailId,Integer.MAX_VALUE);
-        calendar = new ArrayList<>();
+        super(emailId);
+        this.calendar = new ArrayList<>();
     }
 
     public void addMeeting(Meeting meeting){
         //add the meeting to calendar
-     calendar.add(meeting);
+    calendar.add(meeting);
     }
 
     public int findMaxMeetings(){
@@ -27,33 +27,16 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-        ArrayList<Meeting> curr = new ArrayList<>(calendar);
-
-        Collections.sort(curr, Comparator.comparing(Meeting::getStartTime));
-
-        LocalTime prevS = curr.get(0).getStartTime();
-        LocalTime prevE = curr.get(0).getEndTime();
-
-        int count=0;
-
-        for(int i=1;i<curr.size();i++){
-
-            LocalTime currS = curr.get(i).getStartTime();
-            LocalTime currE = curr.get(i).getEndTime();
-
-            if(currS.isAfter(prevE)){
-                count++;
-                prevS = currS;
-                prevE = currE;
-            }else{
-                if(prevE.isAfter((currE))){
-                    prevS = currS;
-                    prevE = currE;
+      int maxMeetings = 0;
+        for (Meeting meeting : calendar) {
+            int count = 0;
+            for (Meeting otherMeeting : calendar) {
+                if (meeting != otherMeeting && meeting.getStartTime().compareTo(otherMeeting.getEndTime()) < 0 && meeting.getEndTime().compareTo(otherMeeting.getStartTime()) > 0) {
+                    count++;
                 }
             }
+            maxMeetings = Math.max(maxMeetings, count);
         }
-
-        return count+1;
-
+        return maxMeetings + 1;
     }
 }
